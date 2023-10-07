6ca406b1e71636da5f0f58480307a29c636c4338
weaknessHero <xleex9843@naver.com>
2023-10-07 20:28:18 +0900

    메인 페이지 양식 구성
    양식 구성 일부 변경

--- my-project2/frontend/src/app/component/home.component.ts
+++ my-project2/frontend/src/app/component/home.component.ts
@@ -3,6 +3,8 @@ import {animate, state, style, transition, trigger} from "@angular/animations";
 import {Router} from "@angular/router";
 import {Observable, of} from "rxjs";
 import {OverlayComponent} from "./overlay/overlay.component";
+import {HttpClient} from "@angular/common/http";
+
 
 @Component({
   selector: 'app-home',
@@ -37,18 +39,55 @@ export class HomeComponent implements OnInit, OnDestroy{
     this.imageRoot + "IMG_4126.PNG",
     this.imageRoot + "제목 7.png"
   ]
+
+  
+  currentIndices = [];
+  posts = [];
+  products=[];
+  communities=[];
+  brands1=[
+    {path:'1',name:'필루미네이트' },
+    {path:'2',name:'나이키' },
+    {path:'3',name:'브라운 브레스' },
+    {path:'4',name:'폴로' },
+    {path:'5',name:'반스' },
+
+    ];
+  brands2=[
+    {path:'6',name:'커버낫' },
+    {path:'7',name:'더블유브이' },
+    {path:'8',name:'Lee' },
+    {path:'9',name:'아커버' },
+    {path:'10',name:'어반드래스' },
+  ];
+
+
   imageLength: number = this.imageList.length;
   currentImage: String;
 
   private slideInterval: any;
 
   constructor(
-    private router: Router
+    private router: Router,
+    private http: HttpClient
   ) {}
 
   ngOnInit(): void{
     this.getImage(this.currentImageId);
     this.autoSlide();
+
+    /*
+    this.http.get(`https://tryon-399311.du.r.appspot.com/posts/all`)
+    .subscribe((data: any[]) => {
+
+      console.log(data);
+      this.posts = data;
+      for (let i = 0; i < data.length; i++) {
+        this.currentIndices[i] = 0;
+      }
+    });
+
+    */
   }
 
   ngOnDestroy(): void{
